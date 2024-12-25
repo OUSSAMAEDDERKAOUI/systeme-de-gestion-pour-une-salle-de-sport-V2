cdee81ecb38433671db16179b54afa05a76567ae
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2024-12-25 22:27:55 +0100

    Règler la formulaire de Sign Up

--- views/login.php
+++ views/login.php
@@ -42,13 +42,13 @@
         <section class="flex flex-col gap-2 bg-black w-[35vw] p-8 rounded-md shadow-sm shadow-orange-500">
             <h1 class="font-semibold text-2xl text-center">WELCOME BACK TO <span class="text-orange-500">OXYFIT</span></h1>
             <h2 class="font-semibold text-xl text-center mb-5">LOGIN FORM</h2>
-            <form action="" class="flex flex-col gap-3">
+            <form method="" action="" class="flex flex-col gap-3">
                 <label for="email">Email Adress</label>
                 <input type="email" name="email" id="email" class="py-1 px-3 text-black placeholder:text-gray-600 outline-none" placeholder="Ex: peter@example.com">
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password" class="py-1 px-3 text-black placeholder:text-gray-600 outline-none" placeholder="Enter Your Password ">
                 <a href="#" class="text-end text-sm underline text-purple-200">Forget Password?</a>
-                <button class="py-1 px-4 bg-orange-500 text-black font-medium mt-5">LOGIN</button>
+                <button name="login" class="py-1 px-4 bg-orange-500 text-black font-medium mt-5">LOGIN</button>
             </form>
         </section>
     </main>
