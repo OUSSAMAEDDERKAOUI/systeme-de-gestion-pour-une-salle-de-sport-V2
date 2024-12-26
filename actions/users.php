b99a957fa1fad18327fd75fc3559931d74f90513
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2024-12-26 13:04:02 +0100

    L'ajout de class user .

---
+++ actions/users.php
@@ -0,0 +1,76 @@
+<?php
+
+class User{
+    protected int $id;
+    protected string  $nom;
+    protected string $prenom;
+    protected string $telephone;
+    protected string $email;
+    protected string $password;
+    protected string $role;
+
+    function __construct($new_nom, $new_prenom, $new_telephone, $new_email, $new_password, $role){
+        $this->nom = $new_nom;
+        $this->prenom = $new_prenom;
+        $this->telephone = $new_telephone;
+        $this->email = $new_email;
+        $this->password = password_hash($new_password, PASSWORD_DEFAULT);
+        $this->role = $role;
+    }
+
+    ////----------> getters :
+
+    function get_id():int{
+        return $this->id;
+
+    }
+    function get_nom():string{
+        return $this->nom;
+
+    } 
+    function get_prenom():string{
+        return $this->prenom;
+
+    } 
+    function get_telephone():string{
+        return $this->telephone;
+
+    } 
+    function get_email():string{
+        return $this->email;
+
+    } 
+    function get_password():string{
+        return $this->password;
+
+    }
+    function get_role():string{
+        return $this->role;
+
+    }
+
+///------> setters
+
+    function set_nom(string $new_nom):void{
+        $this->nom=$new_nom;
+    }
+    function set_id(int $new_id):void{
+        $this->id=$new_id ;
+    }
+    function set_prenom(string $new_prenom):void{
+        $this->prenom=$new_prenom;
+    }
+    function set_telephone(string $new_telephone):void{
+        $this->telephone=$new_telephone ;
+    }
+    function set_email(string $new_email):void{
+        $this->email=$new_email;
+    }
+    function set_password(string $new_password):void{
+        $this->password=password_hash($new_password,PASSWORD_DEFAULT);
+    }
+
+}
+ 
+
+?>
