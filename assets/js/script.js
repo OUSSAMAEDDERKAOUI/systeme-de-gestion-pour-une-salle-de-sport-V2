bb5a15ff5bad732354b687f4ad792674814ed682
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2024-12-27 23:17:17 +0100

    Règler le JS du Dashboard Admi

---
+++ assets/js/script.js
@@ -0,0 +1,96 @@
+const addActivityForm = document.getElementById('add-activity-form');
+const openActivityPopup = document.getElementById('btn-add-activity');
+const btnConfirmActivity = document.getElementById('confirm-add-activity');
+const btnCancelActivity = document.getElementById('cancel-add-activity');
+
+
+btnCancelActivity.addEventListener('click', function(){
+    addActivityForm.classList.add('hidden');
+});
+
+openActivityPopup.addEventListener('click', function(){
+    addActivityForm.classList.remove('hidden');
+});
+
+
+
+
+
+// ADMIN DASHBOARD ========================================
+
+
+const adminManageActivity = document.getElementById('admin-manage-activities');
+const adminManageBooking = document.getElementById('admin-manage-bookings');
+const adminManageMembers = document.getElementById('admin-manage-members');
+const adminManageStatistics = document.getElementById('admin-manage-statistics');
+
+const optionActivity = document.getElementById('admin-activities-option');
+const optionBooking = document.getElementById('admin-bookings-option');
+const optionMembers = document.getElementById('admin-members-option');
+const optionStatistics = document.getElementById('admin-statistics-option');
+
+// const addForm = document.getElementById('add-form');
+// const openPopup = document.getElementById('add-new-booking');
+// const btnConfirm = document.getElementById('confirm-add');
+// const btnCancel = document.getElementById('cancel-add');
+
+
+// btnCancel.addEventListener('click', function(){
+//     addForm.classList.add('hidden');
+// });
+
+// openPopup.addEventListener('click', function(){
+//     addForm.classList.remove('hidden');
+// });
+
+
+optionActivity.addEventListener('click', function(){
+    optionActivity.classList.add('text-orange-500');
+    optionBooking.classList.remove('text-orange-500');
+    optionMembers.classList.remove('text-orange-500');
+    optionStatistics.classList.remove('text-orange-500');
+
+    adminManageActivity.classList.remove('hidden');
+    adminManageBooking.classList.add('hidden');
+    adminManageMembers.classList.add('hidden');
+    adminManageStatistics.classList.add('hidden');
+});
+
+
+optionBooking.addEventListener('click', function(){
+    optionActivity.classList.remove('text-orange-500');
+    optionBooking.classList.add('text-orange-500');
+    optionMembers.classList.remove('text-orange-500');
+    optionStatistics.classList.remove('text-orange-500');
+
+    adminManageActivity.classList.add('hidden');
+    adminManageBooking.classList.remove('hidden');
+    adminManageMembers.classList.add('hidden');
+    adminManageStatistics.classList.add('hidden');
+});
+
+
+optionMembers.addEventListener('click', function(){
+    optionActivity.classList.remove('text-orange-500');
+    optionBooking.classList.remove('text-orange-500');
+    optionMembers.classList.add('text-orange-500');
+    optionStatistics.classList.remove('text-orange-500');
+
+    adminManageActivity.classList.add('hidden');
+    adminManageBooking.classList.add('hidden');
+    adminManageMembers.classList.remove('hidden');
+    adminManageStatistics.classList.add('hidden');
+});
+
+
+optionStatistics.addEventListener('click', function(){
+    optionActivity.classList.remove('text-orange-500');
+    optionBooking.classList.remove('text-orange-500');
+    optionMembers.classList.remove('text-orange-500');
+    optionStatistics.classList.add('text-orange-500');
+
+    adminManageActivity.classList.add('hidden');
+    adminManageBooking.classList.add('hidden');
+    adminManageMembers.classList.add('hidden');
+    adminManageStatistics.classList.remove('hidden');
+});
--- views/dashboard_admin.php
+++ views/dashboard_admin.php
@@ -38,8 +38,7 @@ if ($_SESSION['user_role'] !== 'admin') {
 
         <div class="flex flex-col justify-between flex-1 mt-6">
             <nav>
-                <a class="flex items-center px-4 py-2 text-gray-700 bg-gray-100 rounded-lg dark:bg-black dark:text-gray-200"
-                    href="#">
+                <div id="admin-statistics-option" class="flex items-center px-4 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700 cursor-pointer">
                     <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M19 11H5M19 11C20.1046 11 21 11.8954 21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V13C3 11.8954 3.89543 11 5 11M19 11V9C19 7.89543 18.1046 7 17 7M5 11V9C5 7.89543 5.89543 7 7 7M7 7V5C7 3.89543 7.89543 3 9 3H15C16.1046 3 17 3.89543 17 5V7M7 7H17"
@@ -47,10 +46,9 @@ if ($_SESSION['user_role'] !== 'admin') {
                     </svg>
 
                     <span class="mx-4 font-medium">Statistics</span>
-                </a>
+                </div>
 
-                <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-                    href="#">
+                <div id="admin-members-option" class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700 cursor-pointer">
                     <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z"
@@ -60,10 +58,9 @@ if ($_SESSION['user_role'] !== 'admin') {
                     </svg>
 
                     <span class="mx-4 font-medium">Members</span>
-                </a>
+                </div>
 
-                <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-                    href="#">
+                <div id="admin-activities-option" class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700 cursor-pointer">
                     <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M15 5V7M15 11V13M15 17V19M5 5C3.89543 5 3 5.89543 3 7V10C4.10457 10 5 10.8954 5 12C5 13.1046 4.10457 14 3 14V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V14C19.8954 14 19 13.1046 19 12C19 10.8954 19.8954 10 21 10V7C21 5.89543 20.1046 5 19 5H5Z"
@@ -71,10 +68,9 @@ if ($_SESSION['user_role'] !== 'admin') {
                     </svg>
 
                     <span class="mx-4 font-medium">Activities</span>
-                </a>
+                </div>
 
-                <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-                    href="#">
+                <div id="admin-bookings-option" class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700 cursor-pointer">
                     <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M10.3246 4.31731C10.751 2.5609 13.249 2.5609 13.6754 4.31731C13.9508 5.45193 15.2507 5.99038 16.2478 5.38285C17.7913 4.44239 19.5576 6.2087 18.6172 7.75218C18.0096 8.74925 18.5481 10.0492 19.6827 10.3246C21.4391 10.751 21.4391 13.249 19.6827 13.6754C18.5481 13.9508 18.0096 15.2507 18.6172 16.2478C19.5576 17.7913 17.7913 19.5576 16.2478 18.6172C15.2507 18.0096 13.9508 18.5481 13.6754 19.6827C13.249 21.4391 10.751 21.4391 10.3246 19.6827C10.0492 18.5481 8.74926 18.0096 7.75219 18.6172C6.2087 19.5576 4.44239 17.7913 5.38285 16.2478C5.99038 15.2507 5.45193 13.9508 4.31731 13.6754C2.5609 13.249 2.5609 10.751 4.31731 10.3246C5.45193 10.0492 5.99037 8.74926 5.38285 7.75218C4.44239 6.2087 6.2087 4.44239 7.75219 5.38285C8.74926 5.99037 10.0492 5.45193 10.3246 4.31731Z"
@@ -85,7 +81,7 @@ if ($_SESSION['user_role'] !== 'admin') {
                     </svg>
 
                     <span class="mx-4 font-medium">Bookings</span>
-                </a>
+                </div>
             </nav>
         </div>
 
@@ -98,7 +94,7 @@ if ($_SESSION['user_role'] !== 'admin') {
 
     <main class="w-full bg-gray-200 h-screen text-black">
         <!-- STATISTICS SECTION -->
-        <section id="admin-statistics" class="hidden grid grid-cols-3 gap-10 px-14 py-10 text-center">
+        <section id="admin-manage-statistics" class="grid grid-cols-3 gap-10 px-14 py-10 text-center">
             <div
                 class="flex flex-col gap-8 items-center justify-center p-5 bg-white shadow-sm shadow-gray-500 border-t-8 border-green-500 rounded-md flex-1">
                 <h1 class="text-2xl font-semibold">Confirmed Reservations</h1>
@@ -179,7 +175,7 @@ if ($_SESSION['user_role'] !== 'admin') {
         </section>
 
         <!-- ACTIVITY SECTION -->
-        <section id="admin-activities" class="hidden overflow-auto w-[85vw] h-screen px-5">
+        <section id="admin-manage-activities" class="hidden overflow-auto w-[85vw] h-screen px-5">
             <div class="" id="manage-activity">
                 <div class="flex items-center justify-around py-10">
                     <button id="btn-add-activity" type="button"
@@ -252,7 +248,7 @@ if ($_SESSION['user_role'] !== 'admin') {
 
 
         <!-- MEMBERS SECTION -->
-        <section id="admin-members" class="hidden flex items-center overflow-auto w-[85vw] h-screen px-5">
+        <section id="admin-manage-members" class="hidden flex items-center overflow-auto w-[85vw] h-screen px-5">
             <div class="text-[10px]">
                 <table class="w-[80vw] divide-y divide-gray-200">
                     <tr>
@@ -287,7 +283,7 @@ if ($_SESSION['user_role'] !== 'admin') {
 
 
         <!-- BOOKINGS SECTION -->
-        <section id="admin-booking" class="hidden flex items-center overflow-auto w-[85vw] h-screen px-5">
+        <section id="admin-manage-bookings" class="hidden flex items-center overflow-auto w-[85vw] h-screen px-5">
             <div class="text-[10px]">
                 <table class="w-[80vw] divide-y divide-gray-200">
                     <tr>
@@ -356,6 +352,8 @@ if ($_SESSION['user_role'] !== 'admin') {
         </section>
 
     </main>
+
+    <script src="../assets/js/script.js"></script>
 
 </body>
 </html>
